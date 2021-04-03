341da11b323e8897a318480f58b99ecb58a2605a
Viacheslav Tekaev <tekaeff@yandex.ru>
2021-04-03 14:05:54 +0300

    Add sample project

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <h5>Hi people</h5>
+  </div>
+)
+
+export default IndexPage
