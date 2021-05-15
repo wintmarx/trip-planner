b0c8562722335d084a7ff99cfc6ddc07e5cdd99f
Viacheslav Tekaev <tekaeff@yandex.ru>
2021-05-16 01:22:10 +0300

    Add days division into the timeline

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -15,20 +15,49 @@ export default class Timeline extends React.Component<IProps> {
     super(props)
   }
 
-  getFillWidth() {
-    var perc = Math.round((this.props.time / this.props.dayTime) * 100)
-    if (perc > 100) {
-      return 100
+  getTimeOverflow() {
+    return this.props.time % this.props.dayTime
+  }
+
+  getHrs() {
+    const h = Math.floor(this.getTimeOverflow() / 60)
+    if (this.getTimeOverflow() == 0 && this.props.time > 0) {
+      return Math.floor(this.props.dayTime / 60)
     }
-    return perc
+    return h
+  }
+
+  getMins() {
+    return Math.max(this.getTimeOverflow() - this.getHrs() * 60, 0)
+  }
+
+  getDays() {
+    const d = Math.floor(this.props.time / this.props.dayTime)
+    if (this.getTimeOverflow() == 0) {
+      return d - 1
+    }
+    return d
+  }
+
+  getFillWidth() {
+    const time = this.getMins() + this.getHrs() * 60
+    return Math.min(Math.round((time / this.props.dayTime) * 100), 100)
   }
 
   getTimeString() {
-    const hrs = Math.floor(this.props.time / 60)
-    const mins = this.props.time % 60
+    const hrs = this.getHrs()
+    const mins = this.getMins()
     const hrs_str = `${hrs} ${i18n["hrs"]} `
     const mins_str = `${mins} ${i18n["min"]}`
     return `${hrs > 0 ? hrs_str : ""} ${mins > 0 ? mins_str : ""}`
+  }
+
+  getTimeOverflowString() {
+    return `${this.getDays()} ${i18n["overflow_day"]}`
+  }
+
+  isTimeOverflow() {
+    return this.props.time > this.props.dayTime
   }
 
   isHidden() {
@@ -47,6 +76,12 @@ export default class Timeline extends React.Component<IProps> {
         className="timeline"
       >
         <div className="timeline-container">
+          {this.isTimeOverflow.call(this) && (
+            <div className="timeline-side">
+              <p>{this.getTimeOverflowString.call(this)}</p>
+              <div className="timeline-side-bar" />
+            </div>
+          )}
           <div className="time-bar-container">
             <div className="bar-header">
               <p>{this.getTimeString.call(this)}</p>
