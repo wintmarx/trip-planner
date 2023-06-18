18585cfbe0b5178c8a1e5791f64fc86a6a1d5fab
Viacheslav Tekaev <tekaeff@yandex.ru>
2023-06-18 18:59:59 +0200

    Refactor index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,52 @@
 import React, { useEffect } from "react";
-import {
-  useLocation,
-  Location,
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { useLocation, Location, BrowserRouter, Routes, Route } from "react-router-dom";
 import { App, PlaceData } from "../components/app";
 import { StaticQuery, graphql } from "gatsby";
+
+const CSV_ACTIVITIES_QUERY = graphql`
+  query {
+    allActivitiesCsv {
+      nodes {
+        name
+        description
+        who_add
+        twogis_nav
+        google_nav
+        yandex_nav
+        photo
+        coordinates
+        time_to_visit
+        price
+        working_hours
+        number_of_reviews_in_gm
+        average_gm_rating
+        number_of_ta_reviews
+        average_ta_rating
+        architectural_structure
+        natural_beauty
+        religious_object
+        museum_exhibition
+        park_square
+        art_object
+        little_know_place
+        party
+        entertainment_place
+        _
+        history
+        classical_art
+        contemporary_art
+        music
+        movie
+        literature
+        technologies
+        religion
+        sport
+        fashion
+        photos
+      }
+    }
+  }
+`;
 
 type ActivityCSV = {
   name: string;
@@ -24,6 +63,11 @@ type ActivityCSV = {
   [x: string]: string;
 };
 
+type Tags = {
+  places: string[];
+  themes: string[];
+};
+
 function getQueryVariable(variable: string, location: Location) {
   const query = location.search.substring(1);
   const vars = query.split("&");
@@ -36,105 +80,56 @@ function getQueryVariable(variable: string, location: Location) {
   return false;
 }
 
+function convertActivityToPlaceData(activity: ActivityCSV, tags: Tags) {
+  return {
+    id: activity.photo,
+    name: activity.name,
+    description: activity.description,
+    photo: `photo/${activity.photo}.jpg`,
+    ttv: Number(activity.time_to_visit),
+    gmReviewsCnt: Number(activity.number_of_reviews_in_gm),
+    gmRating: Number(activity.average_gm_rating),
+    taReviewsCnt: Number(activity.number_of_ta_reviews),
+    taRating: Number(activity.average_ta_rating),
+    twogisLink: activity.twogis_nav,
+    googleLink: activity.google_nav,
+    yandexLink: activity.yandex_nav,
+    tags: {
+      places: tags.places.filter((placeTag) => activity[placeTag] == "+"),
+      themes: tags.themes.filter((themeTag) => activity[themeTag] == "+"),
+    },
+  } as PlaceData;
+}
+
+function parseTags(activitiesCSV: ActivityCSV[]) {
+  const keys = Object.keys(activitiesCSV[0]);
+  const beginPos = keys.findIndex((el) => el === "average_ta_rating");
+  const delimPos = keys.findIndex((el) => el === "_");
+  return {
+    places: keys.slice(beginPos + 1, delimPos),
+    themes: keys.slice(delimPos + 1),
+  } as Tags;
+}
+
+function renderApp(data: any) {
+  const activitiesCSV = data.allActivitiesCsv.nodes;
+  const tags = parseTags(activitiesCSV);
+  const places: PlaceData[] = activitiesCSV.map((activity: ActivityCSV) => convertActivityToPlaceData(activity, tags));
+
+  const location = useLocation();
+  const isDebug = typeof getQueryVariable("debug", location) == "string";
+
+  useEffect(() => {
+    document.title = "Trip Planner";
+  }, []);
+
+  return <App debug={isDebug} places={places} tags={tags} />;
+}
+
 const IndexPage: React.FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route
-        path="/"
-        element={
-          <StaticQuery
-            query={graphql`
-              query {
-                allActivitiesCsv {
-                  nodes {
-                    name
-                    description
-                    who_add
-                    twogis_nav
-                    google_nav
-                    yandex_nav
-                    photo
-                    coordinates
-                    time_to_visit
-                    price
-                    working_hours
-                    number_of_reviews_in_gm
-                    average_gm_rating
-                    number_of_ta_reviews
-                    average_ta_rating
-                    architectural_structure
-                    natural_beauty
-                    religious_object
-                    museum_exhibition
-                    park_square
-                    art_object
-                    little_know_place
-                    party
-                    entertainment_place
-                    _
-                    history
-                    classical_art
-                    contemporary_art
-                    music
-                    movie
-                    literature
-                    technologies
-                    religion
-                    sport
-                    fashion
-                    photos
-                  }
-                }
-              }
-            `}
-            render={data => {
-              const activitiesCSV = data.allActivitiesCsv.nodes;
-              const keys = Object.keys(activitiesCSV[0]);
-              const beginPos = keys.findIndex(el => el === "average_ta_rating");
-              const delPos = keys.findIndex(el => el === "_");
-              const tags = {
-                places: keys.slice(beginPos + 1, delPos),
-                themes: keys.slice(delPos + 1),
-              };
-              const places: PlaceData[] = activitiesCSV.map(
-                (el: ActivityCSV) =>
-                  ({
-                    id: el.photo,
-                    name: el.name,
-                    description: el.description,
-                    photo: `photo/${el.photo}.jpg`,
-                    ttv: Number(el.time_to_visit),
-                    gmReviewsCnt: Number(el.number_of_reviews_in_gm),
-                    gmRating: Number(el.average_gm_rating),
-                    taReviewsCnt: Number(el.number_of_ta_reviews),
-                    taRating: Number(el.average_ta_rating),
-                    twogisLink: el.twogis_nav,
-                    googleLink: el.google_nav,
-                    yandexLink: el.yandex_nav,
-                    tags: {
-                      places: tags.places.filter(
-                        placeTag => el[placeTag] == "+"
-                      ),
-                      themes: tags.themes.filter(
-                        themeTag => el[themeTag] == "+"
-                      ),
-                    },
-                  } as PlaceData)
-              );
-
-              const location = useLocation();
-              const isDebug =
-                typeof getQueryVariable("debug", location) == "string";
-
-              useEffect(() => {
-                document.title = "Trip Planner";
-              }, []);
-
-              return <App debug={isDebug} places={places} tags={tags} />;
-            }}
-          />
-        }
-      />
+      <Route path="/" element={<StaticQuery query={CSV_ACTIVITIES_QUERY} render={renderApp} />} />
     </Routes>
   </BrowserRouter>
 );
